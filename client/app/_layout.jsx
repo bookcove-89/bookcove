c5aeb26db7306ad1d72c402ed8614a10d38c47a6
carlosq-mv <cquiroz7869@gmail.com>
2025-02-22 01:39:59 -0800

    project files

---
+++ client/app/_layout.jsx
@@ -0,0 +1,45 @@
+import { Stack } from 'expo-router'
+import { useFonts } from 'expo-font'
+import { useEffect } from 'react';
+import { SplashScreen } from 'expo-router';
+
+// Prevent the splash screen from auto-hiding before asset loading is complete.
+SplashScreen.preventAutoHideAsync();
+
+const RootLayout = () => {
+  const [fontsLoaded, error] = useFonts({
+    "EBG": require("../assets/fonts/EBGaramond-VariableFont_wght.ttf"),
+    "EBG-Italic": require("../assets/fonts/EBGaramond-Italic-VariableFont_wght.ttf"),
+    "Poppins-Black": require("../assets/fonts/Poppins-Black.ttf"),
+    "Poppins-Bold": require("../assets/fonts/Poppins-Bold.ttf"),
+    "Poppins-Medium": require("../assets/fonts/Poppins-Medium.ttf"),
+    "Poppins-Regular": require("../assets/fonts/Poppins-Regular.ttf"),
+    "Poppins-SemiBold": require("../assets/fonts/Poppins-SemiBold.ttf"),
+  });
+  useEffect(() => {
+    if (error) throw error;
+
+    if (fontsLoaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded, error]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
+  if (!fontsLoaded && !error) {
+    return null;
+  }
+  return (
+    <>
+      <Stack>
+        <Stack.Screen name="(onboarding)" options={{ headerShown: false }} />
+        <Stack.Screen name="index" options={{ headerShown: false }} />
+      </Stack>
+    </>
+  )
+}
+
+export default RootLayout
+
