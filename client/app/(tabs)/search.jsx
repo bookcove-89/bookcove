20680578a9210c1c334225288cd43622abeda68c
carlosq-mv <cquiroz7869@gmail.com>
2025-05-23 14:02:46 -0700

    search screen

--- client/app/(tabs)/search.jsx
+++ client/app/(tabs)/search.jsx
@@ -1,29 +1,31 @@
 import {
   ActivityIndicator,
   TouchableOpacity,
-  ScrollView,
   StyleSheet,
   Text,
   View,
-  Image,
   Alert,
+  FlatList,
 } from 'react-native'
+import { router } from 'expo-router'
 import { useState, useEffect } from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
-import { router } from 'expo-router'
 
-import { search, recentSearches, getRecentSearches } from '../../lib/bookapi'
-import Book from '../../components/Book'
 import FormField from '../../components/FormField'
-import SearchIcon from '../../assets/icons/search.svg'
-import BookLogo from '../../assets/images/book.svg'
-import AboutIcon from '../../assets/icons/badge-info.svg'
-import AddIcon from '../../assets/icons/bookmark-plus.svg'
-import HeartIcon from '../../assets/icons/heart.svg'
-import Empty from '../../assets/images/empty.svg'
-import SunRise from '../../assets/images/sun-rise.svg'
+import Header from '../../components/Header'
+import Book from '../../components/Book'
 
+import Empty from '../../assets/images/empty.svg'
+import SearchIcon from '../../assets/icons/search.svg'
+import SunRise from '../../assets/images/sun-rise.svg'
+import AboutIcon from '../../assets/icons/badge-info.svg'
+
+import { useGlobalContext } from '../../context/GlobalProvider'
+import { search, recentSearches, getRecentSearches } from '../../lib/bookapi'
+
+// TODO: Fix favorite rendering issue after adding to favorite in query page
 const Search = () => {
+  const { isLogged, user } = useGlobalContext()
   const [form, setForm] = useState({
     book_name: ''
   })
@@ -46,7 +48,7 @@ const Search = () => {
     } catch (err) {
       console.error(err)
     }
-  }
+  };
 
   const handlePostRecentSearches = async (bookname) => {
     try {
@@ -56,7 +58,7 @@ const Search = () => {
     } catch (err) {
       console.error(err)
     }
-  }
+  };
 
   const submitSearchForm = async (bookname) => {
     setLoading(true);
@@ -73,19 +75,58 @@ const Search = () => {
     }
   };
 
+  const renderBooks = ({ item: book }) => (
+    <View style={styles.bookList} >
+      <View style={styles.bookContainer}>
+        <Book
+          uri={book.cover_img.at(-1)}
+          title={book.title}
+        />
+      </View>
+      <View style={{ marginLeft: 10, flex: 1 }}>
+        <Text style={styles.bookTitle}>{book?.title}</Text>
+        <Text style={[styles.txt, { fontSize: 12 }]}>Author(s): {book?.authors?.join(', ')}</Text>
+
+        <View style={{ paddingTop: 60, paddingHorizontal: 20, alignItems: 'center' }}>
+          <TouchableOpacity
+            onPress={() => router.push({
+              pathname: `search/${book.id}`,
+              params: { book: JSON.stringify(book) }
+            })}
+          >
+            <AboutIcon height={25} width={25} stroke='black' />
+          </TouchableOpacity>
+          <Text style={[styles.txt, { fontSize: 12, paddingTop: 5 }]}>More Info</Text>
+        </View>
+      </View>
+    </View>
+  );
+
+  const renderRecentSearches = ({ item }) => (
+    <>
+      <TouchableOpacity
+        onPress={() => {
+          setForm({ ...form, book_name: item });
+          submitSearchForm(item);
+        }}
+        style={{ flexDirection: 'row', alignItems: 'center', paddingVertical: 2 }}
+      >
+        <SearchIcon height={15} width={15} stroke="#1E90FF" />
+        <Text style={{ color: '#1E90FF', fontFamily: 'Poppins-Regular', marginLeft: 6 }}>
+          {item}
+        </Text>
+      </TouchableOpacity>
+      <View style={{ height: 1, width: '100%', backgroundColor: '#ccc' }} />
+    </>
+  );
+
   return (
     <SafeAreaView style={styles.container}>
       <View>
-
-        <View style={styles.headerContainer}>
-          <View>
-            <Text style={styles.txt}>Book Cove</Text>
-            <Text style={[styles.txtlg, { lineHeight: 27 }]}>
-              Search
-            </Text>
-          </View>
-          <BookLogo height={48} width={48} />
-        </View>
+        <Header
+          smallTxt={"Book Cove"}
+          largeTxt={"Search"}
+        />
 
         <View style={{ padding: 0 }}>
           <FormField
@@ -107,37 +148,14 @@ const Search = () => {
 
               {recentSearchItems.length > 0 ? (
                 <>
-                  {recentSearchItems.map((item, index) => (
-                    <View key={item}>
-                      <TouchableOpacity
-                        onPress={() => {
-                          setForm({ ...form, book_name: item });
-                          submitSearchForm(item);
-                        }}
-                        style={{ flexDirection: 'row', alignItems: 'center', paddingVertical: 2 }}
-                      >
-                        <SearchIcon height={15} width={15} stroke="#1E90FF" />
-                        <Text
-                          style={{
-                            color: '#1E90FF',
-                            fontFamily: 'Poppins-Regular',
-                            marginLeft: 6,
-                          }}
-                        >
-                          {item}
-                        </Text>
-                      </TouchableOpacity>
-
-                      <View
-                        style={{
-                          height: 1,
-                          width: '100%',
-                          backgroundColor: '#ccc',
-                        }}
-                      />
-                    </View>
-                  ))}
-
+                  <FlatList
+                    data={recentSearchItems}
+                    renderItem={renderRecentSearches}
+                    keyExtractor={(item) => item.id ? item.id.toString() : (item._id ? item._id.toString() : Math.random().toString())}
+                    numColumns={1}
+                    showsVerticalScrollIndicator={false}
+                    scrollEnabled={false}
+                  />
                   <View style={{ alignItems: 'center', marginTop: 20 }}>
                     <SunRise height={200} width={300} />
                     <Text style={[styles.txt, { fontSize: 14, color: 'red' }]}>Let today’s search lead to tomorrow’s favorite story.</Text>
@@ -155,70 +173,19 @@ const Search = () => {
               )}
             </View>
           )}
-
         </View>
 
-
-        <ScrollView
-          style={{ paddingRight: 10 }}
-          contentContainerStyle={{ paddingBottom: 100 }}
-        >
-          {res.length > 0 ? (
-            res.map((book, index) => (
-              <View key={index} style={styles.bookList} >
-                <View style={{ flexDirection: 'row' }}>
-
-                  <View style={styles.bookContainer}>
-                    <Book
-                      uri={book.cover_img.at(-1)}
-                      title={book.title}
-                    />
-                  </View>
-
-                  <View style={{ marginLeft: 10, flex: 1 }}>
-                    <Text style={styles.bookTitle}>{book?.title}</Text>
-                    <Text style={[styles.txt, { fontSize: 12 }]}>Author(s): {book?.authors?.join(', ')}</Text>
-
-                    <View style={{ flexDirection: 'row', paddingTop: 60, justifyContent: 'space-between', paddingHorizontal: 20 }}>
-                      <View style={{ alignItems: 'center' }}>
-                        <TouchableOpacity
-                          onPress={() => router.push({
-                            pathname: `search/${book.id}`,
-                            params: { book: JSON.stringify(book) }
-                          })}
-                        >
-                          <AboutIcon height={25} width={25} stroke='black' />
-                        </TouchableOpacity>
-                        <Text style={[styles.txt, { fontSize: 12, paddingTop: 5 }]}>Info</Text>
-                      </View>
-
-                      <View style={{ alignItems: 'center', marginLeft: 10 }}>
-                        <TouchableOpacity>
-                          <AddIcon height={25} width={25} stroke='black' />
-                        </TouchableOpacity>
-                        <Text style={[styles.txt, { fontSize: 12, paddingTop: 5 }]}>Add</Text>
-                      </View>
-
-                      <View style={{ alignItems: 'center' }}>
-                        <TouchableOpacity>
-                          <HeartIcon height={25} width={25} stroke='black' />
-                        </TouchableOpacity>
-                        <Text style={[styles.txt, { fontSize: 12, paddingTop: 5 }]}>Favorite</Text>
-                      </View>
-
-                    </View>
-                  </View>
-
-                </View>
-              </View>
-            ))
-          ) : (
-            <View>
-              <View />
-            </View>
-          )}
-        </ScrollView>
-
+        {res.length > 0 && (
+          <FlatList
+            style={{ height: '100%' }}
+            data={res}
+            renderItem={renderBooks}
+            keyExtractor={(item) => item.id ? item.id.toString() : (item._id ? item._id.toString() : Math.random().toString())}
+            numColumns={1}
+            contentContainerStyle={{ paddingBottom: 100 }}
+            showsVerticalScrollIndicator={false}
+          />
+        )}
       </View >
     </SafeAreaView >
   )
@@ -255,13 +222,11 @@ const styles = StyleSheet.create({
     padding: 8,
     backgroundColor: '#fff',
     borderRadius: 8,
-
     // iOS Shadow
     shadowColor: '#000',
     shadowOffset: { width: 0, height: 2 },
     shadowOpacity: 0.3,
     shadowRadius: 4,
-
     // Android Shadow
     elevation: 5,
   },
@@ -270,5 +235,6 @@ const styles = StyleSheet.create({
     marginBottom: 5,
     backgroundColor: '#E5E5E5',
     borderRadius: 8,
+    flexDirection: 'row'
   }
 })
