5eeea69d11d48ae62d4b1b71d40f278b0d7a13cb
carlosq-mv <cquiroz7869@gmail.com>
2025-05-23 14:03:18 -0700

    root layout

--- client/app/_layout.jsx
+++ client/app/_layout.jsx
@@ -1,6 +1,7 @@
 import { Stack, SplashScreen} from 'expo-router'
 import { useFonts } from 'expo-font'
 import { useEffect } from 'react';
+
 import GlobalProvider from '../context/GlobalProvider';
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
@@ -39,6 +40,7 @@ const RootLayout = () => {
         <Stack>
           <Stack.Screen name="(onboarding)" options={{ headerShown: false }} />
           <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+          <Stack.Screen name="search/[query]" options={{ headerShown: false }} />
           <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
           <Stack.Screen name="index" options={{ headerShown: false }} />
         </Stack>
